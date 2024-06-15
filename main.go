6fb4b40699aa10b2026d36d770ab599950d8cac7
Liscome <62428490+Liscome@users.noreply.github.com>
2024-06-15 04:20:55 +0000

    DBに接続

--- main.go
+++ main.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 )
 
+type TableList struct {
+	Tables_in_h24s17 string `json:"tables_in_h24s17,omitempty" db:"Tables_in_h24s17"`
+}
+
 func main() {
+	conf := mysql.Config{
+		User:                 os.Getenv("MYSQL_USER"),
+		Passwd:               os.Getenv("MYSQL_PASSWORD"),
+		Net:                  "tcp",
+		Addr:                 os.Getenv("MYSQL_HOSTNAME") + ":" + os.Getenv("MYSQL_PORT"),
+		DBName:               "h24s17",
+		AllowNativePasswords: true,
+	}
+
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+		log.Println("ping failed")
+
+		return
+	} else {
+		log.Println("ping succeeded")
+	}
+
+	var x TableList
+	err = db.Get(&x, "SHOW TABLES")
+
+	log.Println(x)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		log.Printf("no table in %s\n", conf.DBName)
+	}
+	if err != nil {
+		log.Fatalf("DB error: %s\n", err)
+
+		return
+	}
+
 	e := echo.New()
 
 	e.GET("/hello/:name", func(c echo.Context) error {
