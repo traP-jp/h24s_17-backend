8fb42c84cfd36a34521fdb522509bac44b871785
Liscome <62428490+Liscome@users.noreply.github.com>
2024-06-15 01:37:51 +0000

    GET /hello/:name の実装(#1)

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+
+	e.GET("/hello/:name", func(c echo.Context) error {
+		userID := c.Param("name")
+		return c.String(http.StatusOK, "Hello, "+userID+".\n")
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
