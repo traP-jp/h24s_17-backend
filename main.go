53e1cce8dd4f2c0111ec67614c10f933b13a94dc
ErrorSyntax1 <ken522.king@gmail.com>
2024-06-15 12:06:33 +0900

    MAC_SECRET関連の追加

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	MAC_SECRET, ok := os.LookupEnv("MAC_SECRET")
+	if !ok {
+		fmt.Println("MAC_SECRET is not set")
+		return
+	}
+	fmt.Printf("loaded MAC_SECRET: %s\n", MAC_SECRET)
+
 	e := echo.New()
 
 	e.GET("/hello/:name", func(c echo.Context) error {
