473f410596db2642f915b730d379cb1e759f57be
itt828 <iittt828@gmail.com>
2024-06-15 13:26:15 +0900

    feat: add /stand endpoint

---
+++ router/gen-token.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+)
+
+type GetTokenResponse struct {
+	token string `json:token`
+}
+
+// tokensテーブルをこのエンドポイント以外からmutateしてはいけない
+// GET /stand?mac_secret=foobar
+func GetTokenhandler(c echo.Context, db sqlx.DB) error {
+	// mac_secretが環境変数と一致
+
+	// tokenをdbから取ってくる
+	token := Token{}
+	err := db.Get(&token, "SELECT * FROM tokens WHERE DATE_ADD(created_at, INTERVAL 5 MINUTE) <= NOW()")
+
+	// 有効なtokenが存在しない場合 作成してinsertする
+	if err != nil {
+		newToken := genToken()
+		_, err := db.Exec("INSERT INTO tokens (token, created_at) VALUES (?, NOW())", newToken)
+		if err != nil {
+			return echo.NewHTTPError(500, "Internal server error")
+		}
+		return c.JSON(200, GetTokenResponse{token})
+	}
+	return c.JSON(200, GetTokenResponse{token})
+}
+
+func genToken() string {
+	return "aiueo"
+}
