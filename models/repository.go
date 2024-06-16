219cf8836d7e1276d7f93eccf32c3c2470a9c36b
Liscome <62428490+Liscome@users.noreply.github.com>
2024-06-16 00:22:42 +0000

    create, readの実装

--- models/repository.go
+++ models/repository.go
@@ -1,9 +1,15 @@
 package models
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/jmoiron/sqlx"
+)
 
 type Repository struct {
 	db *sqlx.DB
+}
+
+type Token struct {
+	Token string `json:"token,omitempty" db:"token"`
 }
 
 func ConnectRepository() (*Repository, error) {
@@ -14,4 +20,20 @@ func ConnectRepository() (*Repository, error) {
 
 func (r *Repository) Migrate() error {
 	return Migrate(r.db)
+}
+
+func (r *Repository) CreateToken(token string) (Token, error) {
+	query := "INSERT INTO tokens (token) VALUES (\"" + token + "\");"
+
+	_, err := r.db.Exec(query)
+
+	return Token{token}, err
+}
+
+func (r *Repository) ReadTokens() ([]Token, error) {
+	var tokens []Token
+	query := "SELECT token FROM tokens"
+	err := r.db.Select(&tokens, query)
+
+	return tokens, err
 }
