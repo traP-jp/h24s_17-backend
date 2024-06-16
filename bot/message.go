00ba91c1f14f8a73d4adb996753c872d8fae831a
ErrorSyntax1 <ken522.king@gmail.com>
2024-06-16 11:18:08 +0900

    Add message.go

---
+++ bot/message.go
@@ -0,0 +1,30 @@
+package bot
+
+import (
+	"image"
+	"log"
+
+	"github.com/traPtitech/go-traq"
+)
+
+type Message struct {
+	strContent *string
+	imgContent *image.Image
+}
+
+func (bot *Bot) sendMessage(cid string, msg *Message, embed bool) {
+	req := traq.NewPostMessageRequest(*msg.strContent)
+	req.Embed = &embed
+	m, r, err := bot.client.MessageApi.
+		PostMessage(bot.auth, cid).
+		PostMessageRequest(*req).
+		Execute()
+	if err != nil {
+		bot.client.MessageApi.
+			PostMessage(bot.auth, cid).
+			PostMessageRequest(*traq.NewPostMessageRequest("Failed To Post Message")).
+			Execute()
+	}
+	log.Println("Sent Message: " + m.Content)
+	log.Printf("StatusCode: %d\n", r.StatusCode)
+}
